fb17883eb493dc8a7ceeec4383eb24c07686acea
javeria Tassawar <163687580+JaveriaaRao@users.noreply.github.com>
2024-11-25 15:07:37 +0500

    Add files via upload

---
+++ LinkedListnode1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class node{
+
+	int data;
+	node* next;
+public:
+	node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+
+	}
+	int getData() {
+		return data;
+	}
+
+	node* getNext() {
+		return next;
+	}
+
+};
+int main()
+{
+	node* node1 = new node(10);
+	cout << node1->getData() << endl;
+	cout << node1->getNext() << endl;
+
+	system("pause");
+    return 0;
+}
